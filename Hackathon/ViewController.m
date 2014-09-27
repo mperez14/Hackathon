fcaeced2dd76810bb49acfd34e77c65a4cb87e2d
Matthew Perez <mperez@Matthews-MacBook-Pro.local>
2014-09-27 18:25:07 -0400

    location working

--- Hackathon/ViewController.m
+++ Hackathon/ViewController.m
@@ -10,37 +10,46 @@
 #import <CoreLocation/CoreLocation.h>
 #define METERS_PER_MILE 1609.344
 
-@interface ViewController () <MKMapViewDelegate> //data
+@interface ViewController () <CLLocationManagerDelegate> //data
+
 
 @end
 
 @implementation ViewController
 
 //logic
-@synthesize mapMain;
+//@synthesize mapMain;
+
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
     [locations lastObject];
 }
 
+
 -(void)viewDidLoad{
     [super viewDidLoad];
     self.mapMain.delegate = self;
-    [self.mapMain setShowsUserLocation:YES];
     
+    self.locationManager = [[CLLocationManager alloc] init];
+    [self.locationManager startUpdatingLocation];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.mapMain.showsUserLocation = YES;
+   [self.mapMain addAnnotation:[self.mapMain userLocation]];
+
+
 }
-/*
+
 //Normal layout
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self.locationManager requestAlwaysAuthorization];
 
 }
-*/
 
 
+/*
 
- // Zoom on stadium
+ // Focus on stadium
 -(void)viewWillAppear:(BOOL)animated {
     [self.locationManager requestAlwaysAuthorization];
     CLLocationCoordinate2D zoomLocation;  //Zoom in on location
@@ -53,7 +62,7 @@
     [mapMain setRegion:viewRegion animated: YES]; //display region
 }
 
-
+*/
 
  //Focus on user
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
@@ -61,36 +70,18 @@
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
     [self.mapMain setRegion:[self.mapMain regionThatFits:region] animated:YES];
     
-    
-    
 }
-/*
-- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
-{
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
-    [self.mapMain setRegion:[self.mapMain regionThatFits:region] animated:YES];
-    
-    
-    // Add an annotation
-    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
-    point.coordinate = userLocation.coordinate;
-    point.title = @"Where am I?";
-    point.subtitle = @"I'm here!!!";
-    
-    [self.mapMain addAnnotation:point];
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id )annotation {
+    if (annotation==mapView.userLocation)
+        return nil;
+    return nil;
 }
-*/
+
+
+
 
 //Show location
 - (IBAction)whereAmI:(id)sender {
-    self.locationManager.delegate = self;
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-
-    [self.locationManager startUpdatingLocation];
-    
-    self.mapMain.showsUserLocation = YES;
-    self.locationManager = [[CLLocationManager alloc] init];
-    [self.mapMain addAnnotation:[self.mapMain userLocation]];
 
     
     
